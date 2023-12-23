af21210fe0487b28c7323b90e03578f5120fb2c3
Ajay Pradhan <pradhanajay14@gmail.com>
2023-12-24 00:26:14 +0530

    nested tag

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const heading = React.createElement("h1", {}, "Hello world from React CDN");
+{
+  /* <div class="parent">
+    <div class="child">
+        <h1>I am h1 tag</h1>
+    </div>
+</div> */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am h1 tag")
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
