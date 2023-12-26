5ff181a39970c900151642e809505a2b0eeeeff7
Ajay Pradhan <pradhanajay14@gmail.com>
2023-12-27 00:36:53 +0530

    heading from react

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
+// React element
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+const heading = React.createElement('h1', { id: 'heading' }, "Namaste React")
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
