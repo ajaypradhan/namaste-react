15182d2674910b3129204ee3ff89d28af6952c71
Ajay Pradhan <pradhanajay14@gmail.com>
2023-12-27 22:40:45 +0530

    heading using jsx

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 // React element
 
-const heading = React.createElement('h1', { id: 'heading' }, "Namaste React")
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+console.log(heading)
+
+// JSX - HTML like or XML like syntax
+const jsxHeading = <h1> Jsx Heading</h1>
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
