0f8b42f90d9372667de8bb6ee7b117380e1855b6
Ajay Pradhan <pradhanajay14@gmail.com>
2023-12-27 23:09:19 +0530

    started with functional component

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ import ReactDOM from "react-dom/client";
 // JSX - HTML like or XML like syntax
 const Heading = <h1> Jsx Heading</h1>;
 
+const HeadingComponent = () => {
+  return <h1>Functional Component</h1>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(Heading);
+root.render(<HeadingComponent/>);
