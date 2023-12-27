c21827ca783c11f23a4b94dda001b7d3ab18bbad
Ajay Pradhan <pradhanajay14@gmail.com>
2023-12-27 22:58:59 +0530

    removed react.createElemen

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element
-
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-
-console.log(heading)
-
 // JSX - HTML like or XML like syntax
-const jsxHeading = <h1> Jsx Heading</h1>
-
-
+const Heading = <h1> Jsx Heading</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(Heading);
